1e896bdfd5378c0c89fda8000845cab48a21d952
Eugene Dubinin <eugend@commandprompt.com>
2024-01-16 15:01:23 +0200

    redmine 5 version

--- init.rb
+++ init.rb
@@ -1,21 +1,14 @@
 require 'redmine'
-
+require_relative 'lib/redmine_rfc822_inline/mail_handler_patch.rb'
 Redmine::Plugin.register :redmine_rfc822_inline do
   name 'Redmine RFC822 inline plugin'
   author 'Eugene Dubinin <eugend@commandprompt.com>'
   author_url 'https://www.commandprompt.com'
   description 'Adds support for inline message/rfc822 mime in inbound emails by converting any inline message parts to text/plain'
-  version '0.2.0'
-  requires_redmine :version_or_higher => '3.4'
+  version '0.2.1'
+  requires_redmine :version_or_higher => '5.0'
   url 'https://github.com/commandprompt/redmine_rfc822'
 end
 
-prepare_block = Proc.new do
-  MailHandler.send(:include, RedmineRFC822Inline::MailHandlerPatch)
-end
 
-if Rails.env.development?
-  ((Rails.version > "5")? ActiveSupport::Reloader : ActionDispatch::Callbacks).to_prepare { prepare_block.call }
-else
-  prepare_block.call
-end
+MailHandler.send(:include, RedmineRFC822Inline::MailHandlerPatch)
